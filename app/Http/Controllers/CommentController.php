b5475f7df8f043d91863f57340ccafe5bff69421
FariqM <maulana.fariq30@gmail.com>
2021-05-22 17:48:36 +0700

    first commit

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\CommentEvent;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function index()
+    {
+        return view('comments');
+    }
+
+    public function fetchComments()
+    {
+
+        $comments = Comment::all();
+
+        return $comments->toJson();
+    }
+
+    public function store(Request $request)
+    {
+        $comment = Comment::create($request->all());
+
+        event(new CommentEvent($comment));
+        return response()->json('ok');
+
+    }
+}
---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+require('./bootstrap')
+
+window.Vue = require("vue").default;
+
+import store from './store/index'
+
+Vue.component("comment", require("./components/Comment").default);
+Vue.component("comments", require("./components/Comments").default);
+Vue.component("new-comment", require("./components/NewComment").default);
+
+const app = new Vue({
+  el: '#app',
+  store
+});
+
+
